1c8d357cb09b279e0520def6ad806c15d9c8ef8f
estefanofc <estefano_fc@hotmail.com>
2020-04-24 03:28:01 -0700

    restart function added

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,42 @@ class _MyAppState extends State<MyApp> {
       'questionText': 'What\'s your favorite color?',
       'answers': [
         {'text': 'Black', 'score': 10},
-        {'tesxt': 'Blue', 'score': 5},
+        {'text': 'Blue', 'score': 5},
         {'text': 'Red', 'score': 3},
         {'text': 'White', 'score': 1},
       ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dog', 'Cat', 'Parrot', 'Tiger']
+      'answers': [
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Parrot', 'score': 3},
+        {'text': 'Tiger', 'score': 1},
+      ]
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Pisan', 'Retik', 'Kool', 'Parsons']
+      'answers': [
+        {'text': 'Pisan', 'score': 10},
+        {'text': 'Retik', 'score': 5},
+        {'text': 'Kool', 'score': 3},
+        {'text': 'Parsons', 'score': 1},
+      ]
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
@@ -57,7 +76,7 @@ class _MyAppState extends State<MyApp> {
                 questions: _questions,
                 questionIndex: _questionIndex,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'Meh';
+    } else if (resultScore <= 12) {
+      resultText = 'Good';
+    } else if (resultScore <= 16) {
+      resultText = 'Excellent';
+    } else {
+      resultText = 'Trash';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
     );
   }
 }
